edf26edcb119d29a66fbfdfc49e5af195bb55a43
ping.develop <zhongyp.develop@gmail.com>
2015-02-25 00:06:20 +0800

    [Demo] v0.5

--- Development/js/app.js
+++ Development/js/app.js
@@ -1,169 +1,93 @@
 (function(){
-	var app = angular.module('thatCleanGirl', [ ]);	
+	var app = angular.module('iwenStudio', [ ]);	
 
-	app.directive('dMultiline',function() {
-		return {			
-			restrict: 'A',
-			require: 'ngModel',
-			scope: {
-				bindModel : '=ngModel',
-			},
-			link: function ($scope, element, attrs) {
-				$scope.$watch(
-                    function( $scope ) {	
-                        if($scope.bindModel){
-							return $scope.bindModel;
-						}else{
-							return "";
-						}
-                    },
-                    function( newValue ) {						
-						if(newValue!=""){
-							element.html(newValue.replace(/\n/g,"<br>"));
-						}
-                    }
-                );	
-			}
-		};
-	});
 	
-	app.directive('dFormControlRotationGroup',function() {
-		return {			
-			restrict: 'A',
-			require: 'ngModel',
-			scope: {
-				bindModel : '=ngModel',
-			},
-			link: function ($scope, element, attrs) {
-				$scope.$watch(
-                    function( $scope ) {	
-                        if($scope.bindModel){
-							return $scope.bindModel;
-						}else{
-							return "";
-						}
-                    },
-                    function( newValue ) {						
-						if(newValue!=""){
-							element.html(newValue.replace(/\n/g,"<br>"));
-						}
-                    }
-                );	
-			}
-		};
-	});
-	
-	app.factory('UserService',function(){
-		var UserService = {
-			account:"jessica",
-			name:"Jessica",
-			role:"Manger"
-		}
-		return UserService;
-	});
 	app.factory('MenuService',function(){
-		var components=[
-				{
-					id:"topMenu1",
-					name:"Client Management",
-					modules:[
-						{
-							id:"client-list",
-							name:"Client List",
-							url:"",
-							isSubModule:false
-						},
-						{
-							id:"new-client",
-							name:"New Client",
-							isSubModule:false
-						}
-					]
-				},
-				{
-					id:"topMenu2",
-					name:"Staff Management",
-					modules:[
-						{
-							id:"staff-list",
-							name:"Staff List",
-							url:"",
-							isSubModule:false
-						}
-					]
-				}
-			];
-		var subModules=[
-			{
-				
-			}
-		]
-		var curComponentIndex = 0;
-		var curModuleIndex = 0;
-		var modulesStack=[];
-		var pushModulesStack=function(selectedIndex,modules){
-				var modulesInfoNode={
-					curModuleIndex:0,
-					modules:modules
-				}
-				modulesStack.push(modulesInfoNode);
-				//console.log(modulesStack);
-		};
-		pushModulesStack(curModuleIndex,components[curComponentIndex].modules);
+		var curComponentIndex = 'home';
 		var MenuService = {
-			initMenuService:function(){
-				console.log("initMenuService");
-				pushModulesStack(0,components[curComponentIndex].modules);
-			},
 			getComponentIndex:function(){
 				return curComponentIndex;
 			},
-			getModuleIndex:function(){
-				return modulesStack[modulesStack.length-1].curModuleIndex;
-			}, 
-			getComponents:function(){
-				return components;
-			},
-			getModules:function(){
-				return modulesStack[modulesStack.length-1].modules;
-			},
-			getActiveModule:function(){
-				var moduleStackNode = modulesStack[modulesStack.length-1];
-				//console.log(moduleStackNode);
-				return moduleStackNode.modules[moduleStackNode.curModuleIndex];
-			},
-			getModulesStackDepth:function(){
-				return modulesStack.length;
-			},
 			changeComponents:function(index){
 				curComponentIndex = index;
-				curModuleIndex=0;
-				modulesStack=[];
-				pushModulesStack(0,components[curComponentIndex].modules);
 			},
-			changeModule:function(index){
-				modulesStack[modulesStack.length-1].curModuleIndex = index;
-			},
-			pushModulesStack:function(modules){
-				pushModulesStack(0,modules);
-			},
-			popModulesStack:function(){
-				if(modulesStack.length>1){
-					modulesStack.pop();
-				}
+			isActive:function(index){
+				return index==curComponentIndex;
 			}
 		}
-		//MenuService.initMenuService();
 		return MenuService;
 	});
 	app.controller('TopMenuController',['$scope','MenuService',function($scope,MenuService){
-		this.components = MenuService.getComponents();
 		$scope.changeComponents = MenuService.changeComponents;
-		$scope.getComponentIndex = MenuService.getComponentIndex;
-		//console.log(this.components);
-
-	}]);	
-	app.controller('SiderController',['$scope','MenuService',function($scope,MenuService){
+		$scope.isActive = MenuService.isActive;
+	}]);
+	
+	app.directive('comContainer',function($compile) {
+		return {
+			restrict: 'E',
+			template: '<div class="container"></div>',
+			scope: {},
+			controller:function($scope,$element,MenuService) {
+				$scope.getComponentIndex = MenuService.getComponentIndex;
+				$scope.$watch(
+                    function( $scope ) {
+                        return $scope.getComponentIndex();
+                    },
+                    function( newValue ) {
+						var html="";
+						if(/^gallery-/i.test(newValue)){
+							console.log(newValue);
+							var galleryId=newValue.substring(8,newValue.length);
+							console.log(galleryId);
+							html="<com-gallery  data-gallery-id='"+galleryId+"'></com-gallery>";
+						}else{
+							html="<com-"+newValue+"></com-"+newValue+">";
+						}
+						var el = $compile( html )( $scope );
+						$element.html( el );
+                    }
+                );				
+			}
+		};
+	});
+	
+	app.directive('comHome',function(){
+		return{
+			restrict: 'E',
+			templateUrl:'directives/comHome.html',
+		};
+	});
+	app.directive('comGallery',function(){
+		return{
+			restrict: 'E',
+			scope: {
+				galleryId:'@galleryId'
+			},
+			controller: function($scope) {
+				console.log($scope.galleryId);
+			},
+			templateUrl:'directives/comGallery.html',
+		};
+	});
+	app.directive('comAbout',function(){
+		return{
+			restrict: 'E',
+			templateUrl:'directives/comAbout.html',
+		};
+	});
+	app.directive('comContact',function(){
+		return{
+			restrict: 'E',
+			templateUrl:'directives/comContact.html',
+		};
+	});
+	app.directive('comCinematography',function(){
+		return{
+			restrict: 'E',
+			templateUrl:'directives/comCinematography.html',
+		};
+	});
+	/* app.controller('SiderController',['$scope','MenuService',function($scope,MenuService){
 		$scope.getModules = MenuService.getModules;
 		$scope.changeModule = MenuService.changeModule;
 		$scope.getModuleIndex = MenuService.getModuleIndex;
@@ -171,30 +95,7 @@
 		$scope.modulesStackDepth = MenuService.getModulesStackDepth;
 	}]);
 	
-	app.directive('moduleContainer',function($compile) {
-		return {
-			restrict: 'E',
-			template: '<div></div>',
-			scope: {},
-			controller:function($scope,$element,MenuService) {
-				$scope.getActiveModule = MenuService.getActiveModule;
-				$scope.$watch(
-                    function( $scope ) {
-                        //console.log( "Function watched" );
-                        // This becomes the value we're "watching".
-						//console.log($scope.getActiveModule());
-                        return $scope.getActiveModule().id;
-                    },
-                    function( newValue ) {
-						if($scope.getActiveModule().isSubModule==false){
-							var el = $compile( "<module-"+newValue+"></module-"+newValue+">" )( $scope );
-							$element.html( el );
-						}	
-                    }
-                );				
-			}
-		};
-	});
+	
 	app.directive('moduleClientList',function() {
 		return {
 			restrict: 'E',
@@ -434,5 +335,5 @@
 			template: '<div>module Staff List</div>',
 			scope: {}
 		};
-	});
+	}); */
 })();
