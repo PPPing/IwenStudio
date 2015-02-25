4e8b9e1a965955447da95297d8b4a964d6941918
PPPing <zhongyp.develop@gmail.com>
2015-02-25 19:13:45 +0800

    [Demo] v0.6

--- Development/js/app.js
+++ Development/js/app.js
@@ -55,6 +55,20 @@
 		return{
 			restrict: 'E',
 			templateUrl:'directives/comHome.html',
+			controller: function($scope,$element,$http,$interval) {
+				var fullScreen=function(element) {
+							if(element.requestFullScreen) {
+								element.requestFullScreen();
+							} else if(element.mozRequestFullScreen) {
+								element.mozRequestFullScreen();
+							} else if(element.webkitRequestFullScreen) {
+							element.webkitRequestFullScreen();
+							}
+				};
+				$scope.launchFullScreen=function(){
+					fullScreen(document.documentElement);
+				};
+			}
 		};
 	});
 	app.directive('comGallery',function(){
@@ -63,8 +77,126 @@
 			scope: {
 				galleryId:'@galleryId'
 			},
-			controller: function($scope) {
+			controller: function($scope,$element,$http,$interval) {
 				console.log($scope.galleryId);
+				$scope.galleryId="test";
+				$scope.gallery=[];
+				$http.get('images/gallery_'+$scope.galleryId+'.json?'+new Date())
+				.then(function(result) {
+					console.log(result);
+					$scope.gallery=result.data;
+				});
+				var timer = 0;
+				var updateTimeInterval=null;
+				var swithImageTimeOut=null;
+				$scope.curImageIndex=0;
+				$scope.slideShowEnable=false;
+				$scope.slideShow=function(index){
+					timer=0;
+					$scope.slideShowEnable=true;
+					$scope.changeFullImage(index);
+					updateTimeInterval=setInterval(updateTimer, 1000);
+				}
+				$scope.autoSlide=true;
+				$scope.slideHide=function(){
+					timer=0;
+					$scope.slideShowEnable=false;
+					if(updateTimeInterval){
+						clearInterval(updateTimeInterval);
+					}
+					if(swithImageTimeOut){
+						clearTimeout(swithImageTimeOut);
+					}
+					$element.find(".gallery-slideShow-fullImage.in").attr("src","");
+					$element.find(".gallery-slideShow-fullImage.out").attr("src","");
+				}
+				$scope.changeFullImage=function(index){
+					console.log(index);
+					console.log($element.find(".gallery-slideShow-loading"));
+					$element.find(".gallery-slideShow-loading").removeClass("hide");
+					if(index>=$scope.gallery.length){
+						index=0;
+					}else if(index<0){
+						index = $scope.gallery.length-1;
+					}
+					$scope.curImageIndex = index;
+					$scope.fullImageCur=$scope.gallery[index].fullImage;
+					var image = $( new Image() ).load(function( event ) {
+									console.log(event);
+									console.log(image);
+									$element.find(".gallery-slideShow-loading").addClass("hide");
+									switchFullImage();
+									
+									if($scope.autoSlide){
+										autoSlideNext(++index);
+									} 
+								}).error(function( event ) {
+									console.log(event);
+								}).prop( "src", $scope.fullImageCur );
+				}
+				
+				var updateTimer=function(){
+					timer++;
+					console.log(timer);
+				};
+				var autoSlideInterval=5;
+				
+				 var autoSlideNext=function(index){
+					 console.log();
+					if(index>=$scope.gallery.length){
+						index=0;
+					}else if(index<0){
+						index = $scope.gallery.length-1;
+					}
+					$scope.curImageIndex = index;
+					$scope.fullImageCur=$scope.gallery[index].fullImage;
+					var image = $( new Image() ).load(function( event ) {
+									if($scope.autoSlide){
+										if(timer>=autoSlideInterval){
+											switchFullImage();
+											autoSlideNext(++index);
+										}else{
+											swithImageTimeOut=setTimeout(function(){
+												console.log("TimeOut");
+												switchFullImage();
+												autoSlideNext(++index);
+											},1000*(autoSlideInterval - timer))
+										}
+									}
+								}).error(function( event ) {
+									console.log(event);
+								}).prop( "src", $scope.fullImageCur );
+				} 
+				
+				var switchFullImage=function(){
+					var imageIn=$element.find(".gallery-slideShow-fullImage.in")
+					var imageOut=$element.find(".gallery-slideShow-fullImage.out");
+									
+					imageOut.attr("src",$scope.fullImageCur);
+					imageOut.attr("src",$scope.fullImageCur).removeClass("out").addClass("in");
+					imageIn.removeClass("in").addClass("out");
+					timer=0;
+				}
+				
+				$scope.toggleAutoSlide=function(){
+					$scope.autoSlide=!$scope.autoSlide;
+					if(swithImageTimeOut){
+						clearTimeout(swithImageTimeOut);
+					}
+					
+					if($scope.autoSlide){
+						autoSlideNext(($scope.curImageIndex+1));
+					}else{
+						
+					}
+				}
+				
+				$scope.nextFullImage=function(){
+					$scope.changeFullImage($scope.curImageIndex+1);
+				}
+				$scope.preFullImage=function(){
+					$scope.changeFullImage($scope.curImageIndex-1);
+				}
 			},
 			templateUrl:'directives/comGallery.html',
 		};
@@ -87,253 +219,4 @@
 			templateUrl:'directives/comCinematography.html',
 		};
 	});
-	/* app.controller('SiderController',['$scope','MenuService',function($scope,MenuService){
-		$scope.getModules = MenuService.getModules;
-		$scope.changeModule = MenuService.changeModule;
-		$scope.getModuleIndex = MenuService.getModuleIndex;
-		$scope.back= MenuService.popModulesStack;
-		$scope.modulesStackDepth = MenuService.getModulesStackDepth;
-	}]);
-	
-	
-	app.directive('moduleClientList',function() {
-		return {
-			restrict: 'E',
-			scope: {},
-			controller: function($scope) {
-				$scope.moduleInfo={
-					curSubmodule:"client-list",
-					clientDetail_clientId:null
-				};
-			},
-			templateUrl:'directives/modules/clientList.html',
-			controllerAs: 'clientListModule'
-		};
-	});
-	
-	app.directive('clientListTmpl',function(){
-		return {
-			restrict: 'E',
-			controller: function($scope,$http,MenuService,UserService) {
-				$http.get('sampleData/clientList.json?timestamp='+ new Date())
-				.then(function(result) {
-					console.log(result);
-					$scope.clientListData = result.data;
-				});
-				this.viewClientDetail=function(clientId){
-					console.log(clientId);
-					console.log($scope.moduleInfo);
-					$scope.moduleInfo.curSubmodule="client-detail";
-					$scope.moduleInfo.clientDetail_clientId = clientId;
-				};
-			},
-			templateUrl:'directives/templates/clientListTmpl.html',
-			controllerAs: 'clientListTmpl'
-		};
-	});
-	
-	app.directive('clientDetailTmpl',function(){
-		return {
-			restrict: 'E',
-			controller: function($scope,$http,MenuService,UserService) {
-				var clientDetailmodules=[
-					{
-						id:"client-detail",
-						name:"Client Detail",
-						isSubModule:true
-					}
-				];
-				$scope.$watch(
-                    function( $scope ) {
-                        return $scope.moduleInfo.clientDetail_clientId;
-                    },
-                    function( newValue ) {
-						newValue = "111";
-						$http.get('sampleData/clientDetail-'+newValue+'.json?timestamp='+ new Date())
-						.then(function(result) {
-							$scope.clientDetail = result.data;
-							$scope.UserService = UserService;
-							$scope.postComment = function(){
-							};
-							MenuService.pushModulesStack(clientDetailmodules);
-						});
-                    }
-                );	
-			},
-			templateUrl:'directives/templates/clientDetailTmpl.html',
-			controllerAs: 'clientDetailTmpl'
-		};
-	});
-	
- 	app.directive('clientInfoSectionTmpl',function(){
-		return{
-			restrict: 'E',
-			require: 'ngModel',
-			scope: {
-				clientDetail : '=ngModel',
-				editMode:'=editMode'
-			},
-			controller: function($scope) {
-				console.log($scope.editMode);
-				$scope.submit=function(){
-					$scope.editMode=false;
-				}
-				$scope.$on('setEidtMode', function (event,eidtMode) {
-					console.log("on setEidtMode : "+eidtMode); // 'Data to send'
-					$scope.editMode = eidtMode
-				});
-			},
-			templateUrl:'directives/templates/clientInfoSectionTmpl.html',
-			controllerAs: 'clientInfoSection'
-		};
-	});
-	
-	app.directive('jobDetailSectionTmpl',function(){
-		return{
-			restrict: 'E',
-			require: 'ngModel',
-			scope: {
-				jobDetails : '=ngModel',
-				editMode:'=editMode'
-			},
-			controller: function($scope) {
-				$scope.submit=function(){
-					$scope.editMode=false;
-				}
-				$scope.$on('setEidtMode', function (event,eidtMode) {
-					console.log("on setEidtMode : "+eidtMode); // 'Data to send'
-					$scope.editMode = eidtMode
-				});
-				this.curItemIndex=0;
-				$scope.addItem=function(){
-						
-				};
-				$scope.editItem=function(index){
-						
-				};
-				$scope.deleteItem=function(index){
-						
-				};
-			},
-			templateUrl:'directives/templates/jobDetailSectionTmpl.html',
-			controllerAs: 'jobDetailSection'
-		};
-	}); 
-	
-	app.directive('paymentSectionTmpl',function(){
-		return{
-			restrict: 'E',
-			require: 'ngModel',
-			scope: {
-				clientDetail : '=ngModel',
-				editMode:'@=ditMode'
-			},
-			controller: function($scope) {	
-				$scope.submit=function(){
-					$scope.editMode=false;
-				}
-				$scope.$on('setEidtMode', function (event,eidtMode) {
-					console.log("on setEidtMode : "+eidtMode); // 'Data to send'
-					$scope.editMode = eidtMode
-				});
-			},
-			templateUrl:'directives/templates/paymentSectionTmpl.html',
-			controllerAs: 'paymentSection'
-		};
-	});
-	
-	app.directive('serviceHistorySectionTmpl',function(){
-		return{
-			restrict: 'E',
-			require: 'ngModel',
-			scope: {
-				clientDetail : '=ngModel',
-				editMode:'@editMode'
-			},
-			controller: function($scope) {	
-				$scope.submit=function(){
-					//console.log($scope.clientDetail);
-					$scope.editMode=false;
-				}
-			},
-			templateUrl:'directives/templates/serviceHistorySectionTmpl.html',
-			controllerAs: 'serviceSection'
-		};
-	});
-	
-	app.directive('commentsSectionTmpl',function($filter){
-		return{
-			restrict: 'E',
-			require: 'ngModel',
-			scope: {
-				comments : '=ngModel',
-			},
-			controller: function($scope,UserService) {
-				$scope.newComment="";
-				$scope.UserService = UserService;
-				$scope.postComment=function(){
-					console.log($scope.newComment);
-					var comment = {
-									content:$scope.newComment, 
-									author:UserService.name, 
-									role:UserService.role, 
-									createDateTime:$filter('date')(new Date(), "yyyy-MM-dd HH:mm:ss")
-								};
-								
-					console.log(comment);
-					$scope.comments.push(comment);
-				};
-				$scope.deleteComment=function(index){
-					//$scope.comments = $filter('orderBy')($scope.comments, '-createDateTime');
-					//console.log($scope.comments);
-					//console.log("index : "+index);
-					//$scope.comments.splice(index,index+1);
-				};
-			},
-			templateUrl:'directives/templates/commentsSectionTmpl.html',
-			controllerAs: 'commentsSection'
-		};
-	});
-	app.directive('moduleNewClient',function() {
-		return {
-			restrict: 'E',
-			templateUrl:'directives/modules/createClient.html',
-			controller: function($scope,$http,$location, $anchorScroll,MenuService) {
-				$scope.hasSubmit = false;
-				$http.get('sampleData/clientDetail-default.json?timestamp='+ new Date())
-					.then(function(result) {
-					$scope.clientDetail=result.data
-					console.log($scope.clientDetail);
-				}); 
-				$scope.Create=function(){
-					$scope.hasSubmit=true;
-					$scope.$broadcast('setEidtMode',$scope.editMode);
-					$location.hash('top');
-					$anchorScroll();
-					
-				};
-				$scope.Reset=function(){
-					$http.get('sampleData/clientDetail-default.json?timestamp='+ new Date())
-						.then(function(result) {
-						$scope.clientDetail=result.data					
-					});
-				};
-				$scope.Confirm=function(){
-					alert("Create new client successfully.");
-					MenuService.changeComponents(0);
-					$location.hash('top');
-					$anchorScroll();
-				};
-			},
-			templateUrl:'directives/modules/createClient.html',
-			controllerAs: 'newClientModule'
-		};
-	});
-	app.directive('moduleStaffList',function() {
-		return {
-			restrict: 'E',
-			template: '<div>module Staff List</div>',
-			scope: {}
-		};
-	}); */
 })();
